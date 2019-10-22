d188e0bddc17a72bab0524d601dd5e90635b263a
ilimones <54744448+ilimones@users.noreply.github.com>
2019-10-21 19:39:39 -0500

    Update LBD.sql

--- Práctica 2/LBD.sql
+++ Práctica 2/LBD.sql
@@ -1,50 +1,58 @@
 create table persona(
-id int not null,
-id_nombre int not null,
-id_domicilio int not null,
+idpersona int not null,
+idnombre int not null,
+iddomicilio int not null,
 edad int not null,
-id_sexo int not null
+idsexo int not null
+constraint pk_idpersona primary key(idpersona desc)
 );
 
 create table nombre(
-id int identity(1,1) not null,
+idnombre int identity(1,1) not null,
 nombre varchar(15) not null,
 appat varchar(15) not null,
 apmat varchar(15) not null,
 nombrecompleto as (nombre+' '+appat+' '+apmat),
-constraint pk_idnombre primary key(id desc)
+constraint pk_idnombre primary key(idnombre desc)
 );
 
 create table domicilio(
-id int identity(1,1) not null,
+iddomicilio int identity(1,1) not null,
 calle varchar (max) not null,
 numint varchar(4) not null,
 numex varchar(4) null,
-id_colonia int not null,
-id_estado int not null,
-domiciliocompleto as (calle+' '+numint+' '+numex+', '+idcolonia+', '+idestado),
-constraint pk_iddomicilio primary key(id desc)
+idcolonia int not null,
+domiciliocompleto as (calle+' '+numint+' '+numex+', '+idcolonia),
+constraint pk_iddomicilio primary key(iddomicilio desc)
 );
 
 create table colonia(
-id int identity(1,1) not null,
+idcolonia int identity(1,1) not null,
 nombre varchar(max) not null,
-id_ciudad int not null,
-constraint pk_idcolonia primary key(id desc)
+idciudad int not null,
+constraint pk_idcolonia primary key(idcolonia desc)
+);
+
+create table ciudad(
+idciudad int identity(1,1) not null,
+nombre varchar(max) not null,
+idestado int not null,
+constraint pk_idciudad primary key(idciudad desc)
 );
 
 create table estado
 (
-id int identity(1,1)  not null,
+idestado int identity(1,1)  not null,
 nombre varchar(max) not null,
-constraint pk_idestado primary key(id desc)
+constraint pk_idestado primary key(idestado desc)
+);
 
 create table sexo
 (
-id int not null,
+idsexo int not null,
 sexo varchar(9)
-constraint pk_idsexo primary key (id desc)
+constraint pk_idsexo primary key (idsexo desc)
 );
 
-create index nombre on nombre (id,nombrecompleto desc);
-create index domicilio on domicilio (id,domiciliocompleto desc);
+create index nombre on nombre (idnombre,nombrecompleto desc);
+create index domicilio on domicilio (iddomicilio,domiciliocompleto desc);
